b2c5e660066629ace29aa9258cfbd0bbc962ad6d
github username <github email address>
2020-06-25 11:57:53 -0400

    finally got active storage fully working

--- app/controllers/api/v1/mushrooms_controller.rb
+++ app/controllers/api/v1/mushrooms_controller.rb
@@ -1,13 +1,21 @@
 class Api::V1::MushroomsController < ApplicationController
     
             def index
-                mushrooms = Mushroom.all 
-                render json: mushrooms
+                mushrooms = Mushroom.all.with_attached_image
+                render json: mushrooms.map { |mushroom|
+                mushroom.as_json.merge({ image: url_for(mushroom.image) })
+                }
             end
     
             def show
                 mushroom = Mushroom.find(params[:id])   
-                render json: mushroom 
+                image = rails_blob_path(mushroom.image)
+
+                if mushroom.id == params[:id]
+                    render json: { mushroom: mushroom, image: image } 
+                else 
+                    render json: mushroom
+                end 
             end
       
     end
