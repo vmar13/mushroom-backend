9e2649d92b02163320d38e5ad41729f21fa53b68
github username <github email address>
2020-06-26 14:27:05 -0400

    stuck on filter images

--- app/controllers/api/v1/health_benefits_controller.rb
+++ app/controllers/api/v1/health_benefits_controller.rb
@@ -3,12 +3,20 @@ class Api::V1::HealthBenefitsController < ApplicationController
     def index
         health_benefits = HealthBenefit.all 
         render json: health_benefits
+        
+        # .to_json(:include => 
+        # {:mushrooms => {:only => [:name, :scientific_name, :image, :location, :flavor]}}, except: [:created_at, :updated_at])
+  
     end
 
     def show
-        health_benefit = HealthBenefit.find(params[:id])   
-        render json: health_benefit 
+        health_benefit = HealthBenefit.find(params[:id]) 
+        render json: health_benefit, include: [:mushrooms]
+        # render json: health_benefit, include: [:mushrooms]
+        # .map { |mushroom|
+        # mushroom.as_json.merge({ image: url_for(mushroom.image) })
+        # }   
     end
 
-    
+        
 end
--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -5,4 +5,11 @@ class Mushroom < ApplicationRecord
     has_many :mush_health_benefits
     has_many :health_benefits, through: :mush_health_benefits 
     has_one_attached :image
+    
+
+    # def as_json(*)
+    #     super.except("created_at", "updated_at").tap do |hash|
+    #       hash["image"] = url_for(hash.image)
+    #     end
+    #   end
 end
