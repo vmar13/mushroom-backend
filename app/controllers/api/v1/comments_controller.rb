556ba0f0d83de3bdbe1554a5ea5adc5fbb2a827d
github username <github email address>
2020-06-28 01:10:15 -0400

    built out comments backend stuff

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CommentsController < ApplicationController
     
             def index
                 comments = Comment.all 
-                render json: comments
+                render json: comments, include: [:mushroom]
             end
     
             def show
