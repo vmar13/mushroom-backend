7b3e55e6f3784df46612fd3e3e601f764c14fa74
github username <60378440+vmar13@users.noreply.github.com>
2021-05-10 13:23:28 -0400

    added validations to comment and error message to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -16,8 +16,12 @@ class Api::V1::CommentsController < ApplicationController
             end 
     
             def create
-                comment = Comment.create(comment_params)
+                comment = Comment.create!(comment_params)
+                if comment.valid?
                 render json: comment
+                else 
+                    render json: { error: 'Please write a comment.' }
+                end
             end 
     
             def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :mushroom
     belongs_to :user
+    # validates :content, presence: { message: 'Please enter a comment.' }
+    validates :content, presence: true
 end
